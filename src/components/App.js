606aabbb94cad2e2d292e4aabc39d3f89f5b3844
Lipika Mohanty <110726592+Lipika07@users.noreply.github.com>
2023-01-28 11:23:32 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -3,16 +3,23 @@ import '../styles/App.css';
 import {useSelector,useDispatch} from "react-redux";
 import {changeColor,changeStyle } from '../actions/index.js';
 
+
 const App = () => {
 
 const dispatch =useDispatch();
-//code here 
+//code here
+ const colorState = useSelector((state)=> state.colorReducer )
+ const styleState = useSelector((state)=> state.styleReducer )
+
+
+
+
 
   return (
     <div id="main">
       <h1 id='text' style={{color: colorState, fontFamily: styleState}}>Newton School</h1>
-      <button id='colorBtn' onClick={()=>dispatch(changeColor(colorState))}>Change Color</button>
-      <button id='styleBtn' onClick={()=>dispatch(changeStyle(styleState))}>Change Style</button>
+      <button id='colorBtn' onClick={()=> dispatch(changeColor(colorState))}>Change Color</button>
+      <button id='styleBtn' onClick={()=> dispatch(changeStyle(styleState))}>Change Style</button>
 
     </div>
   )
